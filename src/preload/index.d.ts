78ea84eed9b7de94c9ef367d32fcd571f1658caf
yaroofie <yaroofie@gmail.com>
2024-02-06 10:34:48 -0800

    refactor : project structure

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -2,7 +2,8 @@ import { ElectronAPI } from '@electron-toolkit/preload'
 
 declare global {
   interface Window {
-    electron: ElectronAPI
-    api: unknown
+    context: {
+      hello: () => void
+    }
   }
 }
